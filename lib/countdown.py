40b084ca39e3ac484d915aa514820eae222c4a52
bryan-kiplangat <kipbryan.bett@gmail.com>
2023-11-26 19:27:51 +0300

    completed assignment

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
-# your code goes here!
+#         '''counts down from number and prints "HAPPY NEW YEAR!"'''
+import time
+
+def countdown(number):
+    while number >= 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        if number == 0:
+            print("HAPPY NEW YEAR!")
+            break
+
+def countdown_with_sleep(number):
+    while number >= 0:
+        print(f'{number} SECOND(S)!')
+        number -= 1
+        time.sleep(1)
+        if number == 0:
+            print("HAPPY NEW YEAR!")
+            break
